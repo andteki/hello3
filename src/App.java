9a678f2256fbf09194d7fb0dac186530839c31b1
Sallai András <andrewteki@gmail.com>
2021-09-06 09:07:26 +0200

    Init

---
+++ src/App.java
@@ -0,0 +1,14 @@
+/*
+* File: App.java
+* Author: Nagy János
+* Copyright: 2021, Nagy János
+* Group: Szoft II N
+* Date: 2021-09-06
+* Github: https://github.com/janos/
+* Licenc: GNU GPL
+*/
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
